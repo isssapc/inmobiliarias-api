02c563b4d759628adb953383bbdb9caa4ad718e5
Ramiro Arechar <ramiro@arechar>
2017-11-23 14:06:25 -0600

    update bd

--- DBscripts/scripts.sql
+++ DBscripts/scripts.sql
@@ -32,6 +32,16 @@ CREATE TABLE `caracteristica` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `caracteristica`
+--
+
+LOCK TABLES `caracteristica` WRITE;
+/*!40000 ALTER TABLE `caracteristica` DISABLE KEYS */;
+INSERT INTO `caracteristica` VALUES (1,'BalcÃ³n',1,1),(2,'Cisterna',1,1),(3,'Estacionamiento Techado',1,1),(4,'Facilidad Estacionamiento',1,1),(5,'Garaje',1,1),(6,'JardÃ­n',1,1),(7,'Parrilla',1,1),(8,'Patio',1,1),(9,'Roof Garden',1,1),(10,'Terraza',1,1),(11,'Accesibilidad',1,3),(12,'Alberca',1,3),(13,'Mascotas',1,4);
+/*!40000 ALTER TABLE `caracteristica` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `documento`
 --
 
@@ -49,6 +59,16 @@ CREATE TABLE `documento` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `documento`
+--
+
+LOCK TABLES `documento` WRITE;
+/*!40000 ALTER TABLE `documento` DISABLE KEYS */;
+INSERT INTO `documento` VALUES (1,1,'path1','hola propietario',0),(2,1,'path2','hola conyuge',1);
+/*!40000 ALTER TABLE `documento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `inmobiliaria`
 --
 
@@ -62,6 +82,16 @@ CREATE TABLE `inmobiliaria` (
   PRIMARY KEY (`id_inmobiliaria`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inmobiliaria`
+--
+
+LOCK TABLES `inmobiliaria` WRITE;
+/*!40000 ALTER TABLE `inmobiliaria` DISABLE KEYS */;
+INSERT INTO `inmobiliaria` VALUES (1,'Realty World',2);
+/*!40000 ALTER TABLE `inmobiliaria` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `mensaje`
@@ -80,6 +110,15 @@ CREATE TABLE `mensaje` (
   PRIMARY KEY (`id_mensaje`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mensaje`
+--
+
+LOCK TABLES `mensaje` WRITE;
+/*!40000 ALTER TABLE `mensaje` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mensaje` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `propiedad`
@@ -182,6 +221,16 @@ CREATE TABLE `propiedad` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `propiedad`
+--
+
+LOCK TABLES `propiedad` WRITE;
+/*!40000 ALTER TABLE `propiedad` DISABLE KEYS */;
+INSERT INTO `propiedad` VALUES (77,'C0001',NULL,0,0,1,1000000.00,13000.00,NULL,20000.00,NULL,3,'Hermosa y amplia casa','Casa en venta',NULL,'200','200',NULL,2,1,NULL,4,89,'Planta Baja',2,'Av. QuerÃ©taro','210',NULL,'Calle Guanajuato','29000',NULL,'Chiapas','Tuxtla GutiÃ©rrez',NULL,NULL,NULL,NULL,NULL,1,'mÂ²','10','20','2','2001','2000','0001','Fraccionamiento La Hacienda',1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,'Precio total','Precio total','2017-08-10',1),(79,'D0001',NULL,1,1,NULL,3000.00,3000.00,NULL,NULL,NULL,6,'Amplio y bonito departamento en el primer cuadro de la ciudad.','Departamento en venta o renta',NULL,'200','200',NULL,2,NULL,NULL,2,4,'4',1,'3ra poniente norte','320','8A','3ra norte poniente','29000',NULL,'Chiapas','Tuxtla Gutierrez',NULL,NULL,NULL,NULL,NULL,1,'mÂ²','10','20','1','1998','500','0002','Centro',1,NULL,NULL,1,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,1,NULL,1,NULL,1,'Precio total','Precio total','2017-08-10',3),(80,'',NULL,1,0,0,6000.00,NULL,NULL,NULL,NULL,15,'Lugar increible','Aprovecha Oportunidad!!!',NULL,'','500',NULL,2,1,NULL,5,NULL,NULL,1,'','',NULL,NULL,'',NULL,'Chiapas','San Cristobal de las Casas',NULL,NULL,NULL,NULL,NULL,1,'ha','','','1','','','',NULL,NULL,0,0,0,NULL,0,0,0,NULL,NULL,0,0,NULL,NULL,NULL,0,0,0,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL,0,0,NULL,NULL,NULL,NULL,0,NULL,0,NULL,1,'Precio total',NULL,'2017-08-10',NULL);
+/*!40000 ALTER TABLE `propiedad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `propiedad_caracteristica`
 --
 
@@ -195,6 +244,15 @@ CREATE TABLE `propiedad_caracteristica` (
   PRIMARY KEY (`id_propiedad_caracteristica`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `propiedad_caracteristica`
+--
+
+LOCK TABLES `propiedad_caracteristica` WRITE;
+/*!40000 ALTER TABLE `propiedad_caracteristica` DISABLE KEYS */;
+/*!40000 ALTER TABLE `propiedad_caracteristica` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `propiedad_imagen`
@@ -217,6 +275,16 @@ CREATE TABLE `propiedad_imagen` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `propiedad_imagen`
+--
+
+LOCK TABLES `propiedad_imagen` WRITE;
+/*!40000 ALTER TABLE `propiedad_imagen` DISABLE KEYS */;
+INSERT INTO `propiedad_imagen` VALUES (48,79,'http://localhost:8080/inmobiliaria/api/upload/propiedades/79/','bg7',NULL,'.jpg',NULL,1),(49,79,'http://localhost:8080/inmobiliaria/api/upload/propiedades/79/','bg8',NULL,'.jpg',NULL,0),(59,80,'http://localhost:8080/inmobiliaria/api/upload/propiedades/80/','horizontal2',NULL,'.jpg',NULL,1),(61,80,'http://localhost:8080/inmobiliaria/api/upload/propiedades/80/','vertical',NULL,'.jpg',NULL,0),(63,77,'http://localhost:8080/inmobiliaria/api/upload/propiedades/77/','vertical',NULL,'.jpg',NULL,0),(64,77,'http://localhost:8080/inmobiliaria/api/upload/propiedades/77/','horizontal',NULL,'.jpg',NULL,1);
+/*!40000 ALTER TABLE `propiedad_imagen` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `propiedad_seguimiento`
 --
 
@@ -232,6 +300,16 @@ CREATE TABLE `propiedad_seguimiento` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `propiedad_seguimiento`
+--
+
+LOCK TABLES `propiedad_seguimiento` WRITE;
+/*!40000 ALTER TABLE `propiedad_seguimiento` DISABLE KEYS */;
+INSERT INTO `propiedad_seguimiento` VALUES (12,77,3,'2017-10-25 18:28:55','hola'),(13,77,3,'2017-10-25 18:29:57','segundo'),(14,77,3,'2017-10-25 18:30:48','tercero'),(15,77,3,'2017-10-25 18:31:59','cuarto'),(16,77,3,'2017-10-25 18:34:22','quinto'),(17,79,3,'2017-10-25 18:47:58','hola mundo!!'),(18,77,3,'2017-11-21 19:17:39','otro '),(19,77,3,'2017-11-21 19:17:53','hola y adios'),(20,77,3,'2017-11-21 19:18:05','hasta la vista'),(21,77,3,'2017-11-21 19:18:13','baby'),(22,77,3,'2017-11-21 19:20:58','un renglÃ³n,\ndos\ntres\ncuatro'),(23,77,3,'2017-11-21 19:21:57','uno');
+/*!40000 ALTER TABLE `propiedad_seguimiento` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `propietario`
@@ -343,6 +421,16 @@ CREATE TABLE `propietario` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `propietario`
+--
+
+LOCK TABLES `propietario` WRITE;
+/*!40000 ALTER TABLE `propietario` DISABLE KEYS */;
+INSERT INTO `propietario` VALUES (1,0,'Ramiro JimÃ©nez','Mexicana','MÃ©xico DF','2017-10-21',NULL,NULL,NULL,NULL,NULL,'(333)444-4444','(333)333-3333','ramiro.arechar@gmail.com',NULL,NULL,'jiar-800502-2p2',NULL,1,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'ingeniero',NULL,'isssa',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'2017-10-14',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,NULL,NULL),(2,0,'Rafael',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'rafael@hotmail.com',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,NULL,NULL),(3,0,'Ramon','Mexicana','Tuxtla',NULL,NULL,NULL,'TerÃ¡n',NULL,NULL,NULL,NULL,'ramon@gmail.com',NULL,NULL,NULL,NULL,1,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,NULL,NULL),(4,0,'JZ Desarrollos',NULL,NULL,'2017-10-13',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'2017-10-02',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'2017-10-29',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'2017-10-03',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,NULL,NULL);
+/*!40000 ALTER TABLE `propietario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `prospecto`
 --
 
@@ -378,6 +466,16 @@ CREATE TABLE `prospecto` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `prospecto`
+--
+
+LOCK TABLES `prospecto` WRITE;
+/*!40000 ALTER TABLE `prospecto` DISABLE KEYS */;
+INSERT INTO `prospecto` VALUES (3,'Ramiro JimÃ©nez ArÃ©char','ingeniero','jz','administrador','Facebook','9999','6666','00000','ramiro.arechar@gmail.com','Av. Oaxaca #20 residencial Hacienda',4,'uno, dos, tres','notasssss',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(15,'prospecto 1',NULL,NULL,NULL,'Facebook',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'1','Casa',NULL,NULL,'3','3',NULL,NULL,NULL),(16,'prospecto 1',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'1','Casa',NULL,NULL,'3','3',NULL,NULL,NULL),(23,'Prospecto Mayor','MÃ©dico Cirujano',NULL,NULL,'Anuncios',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(24,'Futbolista','pumas',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `prospecto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `prospecto_seguimiento`
 --
 
@@ -395,6 +493,16 @@ CREATE TABLE `prospecto_seguimiento` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `prospecto_seguimiento`
+--
+
+LOCK TABLES `prospecto_seguimiento` WRITE;
+/*!40000 ALTER TABLE `prospecto_seguimiento` DISABLE KEYS */;
+INSERT INTO `prospecto_seguimiento` VALUES (1,3,3,'2017-11-21 19:12:24','hola mundo !!!'),(2,3,3,'2017-11-21 19:13:47','mas seguimiento'),(3,3,3,'2017-11-21 19:14:21','Seguimiento mÃ¡s largo que ocupe mas de dos filas para ver cÃ³mo se ve en el componente.\nA ver si respeta los saltos de lÃ­nea.');
+/*!40000 ALTER TABLE `prospecto_seguimiento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `rol`
 --
 
@@ -407,6 +515,16 @@ CREATE TABLE `rol` (
   PRIMARY KEY (`id_rol`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rol`
+--
+
+LOCK TABLES `rol` WRITE;
+/*!40000 ALTER TABLE `rol` DISABLE KEYS */;
+INSERT INTO `rol` VALUES (1,'Administrador'),(2,'Asesor'),(3,'Recepcion Llamadas');
+/*!40000 ALTER TABLE `rol` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tipo_caracteristica`
@@ -423,6 +541,16 @@ CREATE TABLE `tipo_caracteristica` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `tipo_caracteristica`
+--
+
+LOCK TABLES `tipo_caracteristica` WRITE;
+/*!40000 ALTER TABLE `tipo_caracteristica` DISABLE KEYS */;
+INSERT INTO `tipo_caracteristica` VALUES (1,'Exterior'),(2,'Interior'),(3,'RecreaciÃ³n'),(4,'Restricciones');
+/*!40000 ALTER TABLE `tipo_caracteristica` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tipo_documento`
 --
 
@@ -437,6 +565,15 @@ CREATE TABLE `tipo_documento` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `tipo_documento`
+--
+
+LOCK TABLES `tipo_documento` WRITE;
+/*!40000 ALTER TABLE `tipo_documento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tipo_documento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tipo_propiedad`
 --
 
@@ -449,6 +586,16 @@ CREATE TABLE `tipo_propiedad` (
   PRIMARY KEY (`id_tipo_propiedad`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipo_propiedad`
+--
+
+LOCK TABLES `tipo_propiedad` WRITE;
+/*!40000 ALTER TABLE `tipo_propiedad` DISABLE KEYS */;
+INSERT INTO `tipo_propiedad` VALUES (1,'Bodega comercial'),(2,'Bodega industrial'),(3,'Casa'),(4,'Casa con uso de suelo'),(5,'Casa en condominio'),(6,'Departamento'),(7,'Edificio'),(8,'Huerta'),(9,'Local'),(10,'Local comercial'),(11,'Local en centro comercial'),(12,'Nave industrial'),(13,'Oficina'),(14,'Otro'),(15,'Quinta'),(16,'Rancho'),(17,'Terreno'),(18,'Terreno comercial'),(19,'Villa');
+/*!40000 ALTER TABLE `tipo_propiedad` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `usuario`
@@ -469,6 +616,16 @@ CREATE TABLE `usuario` (
   UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Emilio Sol','sol6902@hotmail.com','hola','',2,NULL),(3,'Ramiro Jimenez','ramiro.arechar@gmail.com','hola','',1,NULL),(5,'Lionel Messi','messi@barcelona.com','hola','',1,NULL);
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -479,4 +636,4 @@ CREATE TABLE `usuario` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-23 14:02:13
+-- Dump completed on 2017-11-23 14:05:20
