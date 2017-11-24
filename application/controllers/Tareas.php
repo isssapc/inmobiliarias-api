1cb87980fb750778c7d21b4a4afdc6f07cab431f
Ramiro Arechar <ramiro@arechar>
2017-11-24 16:03:41 -0600

    tareas controller

---
+++ application/controllers/Tareas.php
@@ -0,0 +1,52 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tareas extends MY_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model("mensaje");
+    }
+
+    public function usuario_get($id_usuario) {
+        $recibidos = $this->mensaje->get_mensajes_recibidos_usuario($id_usuario);
+        $enviados = $this->mensaje->get_mensajes_enviados_usuario($id_usuario);
+        $this->response(array('recibidos' => $recibidos, 'enviados' => $enviados));
+    }
+
+    public function del_mensajes_usuario_post($id_usuario) {
+        $count = $this->mensaje->del_mensajes_usuario($id_usuario);
+        $this->response(array('count' => $datos));
+    }
+
+    public function get_mensaje_get($id) {
+        $datos = $this->mensaje->get_one($id);
+        $this->response($datos);
+    }
+
+//    
+//    
+//    public function search_mensaje_get($nombre) {
+//        $datos = $this->mensaje->search_by_nombre($nombre);
+//        $this->response($datos);
+//    }
+
+    public function del_mensaje_post($id) {
+        $count = $this->mensaje->del_one($id);
+        $this->response(array("count" => $count));
+    }
+
+    public function create_mensaje_post() {
+        $mensaje = $this->post("mensaje");
+        $datos = $this->mensaje->create_one($mensaje);
+        $this->response($datos);
+    }
+
+    public function update_mensaje_post($id) {
+        $mensaje = $this->post("mensaje");
+        $datos = $this->mensaje->update_one($id, $mensaje);
+        $this->response($datos);
+    }
+
+}
